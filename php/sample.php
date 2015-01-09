1249017b4ba4cf8c2944b5567511371ca2519c57
iceinto <iceinto@163.com>
2015-01-09 19:41:21 +0800

    初始化项目

---
+++ php/sample.php
@@ -0,0 +1,30 @@
+<?php
+require_once "jssdk.php";
+$jssdk = new JSSDK("yourAppID", "yourAppSecret");
+$signPackage = $jssdk->GetSignPackage();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title></title>
+</head>
+<body>
+  
+</body>
+<script src="http://res.wx.qq.com/open/js/jweixin-1.0.0.js"></script>
+<script>
+  wx.config({
+    appId: '<?php echo $signPackage["appId"];?>',
+    timestamp: <?php echo $signPackage["timestamp"];?>,
+    nonceStr: '<?php echo $signPackage["nonceStr"];?>',
+    signature: '<?php echo $signPackage["signature"];?>',
+    jsApiList: [
+      // 所有要调用的 API 都要加到这个列表中
+    ]
+  });
+  wx.ready(function () {
+    // 在这里调用 API
+  });
+</script>
+</html>
