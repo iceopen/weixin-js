1278db754bda70c06759e16c72cffe2a83df6929
iceinto <iceinto@163.com>
2015-04-13 16:44:04 +0800

    改造

--- README.md
+++ README.md
@@ -1,10 +1,116 @@
-## 说明
-JAVA, Node, Python 部分代码只实现了签名算法，需要开发者传入 jsapi_ticket 和 url ，其中 jsapi_ticket 需要通过 http://api.weixin.qq.com/cgi-bin/ticket/getticket?type=jsapi&access_token=ACCESS_TOKEN 接口获取，url 为调用页面的完整 url 。
+声明：由于时间有限，目前仅对微信一部分接口进行了封装，陆续会持续增加封装，敬请关注！
 
-PHP 部分代码包括了获取 access_token 和 jsapi_ticket 的操作，只需传入 appid 和 appsecret 即可，但要注意如果已有其他业务需要使用 access_token 的话，应修改获取 access_token 部分代码从全局缓存中获取，防止重复获取 access_token ，超过调用频率。
+微信公众平台 WeixinApi 感谢[WeixinApi](https://github.com/zxlie/WeixinApi) 因为使用习惯
 
-注意事项：
-1. jsapi_ticket 的有效期为 7200 秒，开发者必须全局缓存 jsapi_ticket ，防止超过调用频率。
+WeixinApi 的诞生
 
-## 说明文档地址
-http://mp.weixin.qq.com/wiki/7/aaa137b55fb2e0456bf8dd9148dd613f.html
+腾讯官方于2015年1月9日发布了微信的JS-SDK，详细接口可以参考：见[微信JS-SDK说明文档](http://mp.weixin.qq.com/wiki/7/aaa137b55fb2e0456bf8dd9148dd613f.html)
+由于微信官方的接口互相独立，但是在事件使用中，我们可能会是很多接口一起使用，所以直接使用官方的接口比较麻烦，重复代码相对较多。为了更方便的使用，我对官方的接口进行了简单的封装，并开源处理，方便大家使用。
+
+
+快速使用
+1、引入js文件
+由于该接口是对微信官方js的封装，需要同时引入微信官方的js-sdk文件和JWeixinApi的js文件。引入如下两个文件：
+jweixin-1.0.0.js
+WeixinApi.js
+
+2、通过config接口注入权限验证配置
+该步骤和官方文档一样，详情[微信JS-SDK说明文档](http://mp.weixin.qq.com/wiki/7/aaa137b55fb2e0456bf8dd9148dd613f.html#.E6.AD.A5.E9.AA.A4.E4.BA.8C.EF.BC.9A.E9.80.9A.E8.BF.87config.E6.8E.A5.E5.8F.A3.E6.B3.A8.E5.85.A5.E6.9D.83.E9.99.90.E9.AA.8C.E8.AF.81.E9.85.8D.E7.BD.AE)
+```javascript
+wx.config({
+    debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+    appId: '', // 必填，公众号的唯一标识
+    timestamp: , // 必填，生成签名的时间戳
+    nonceStr: '', // 必填，生成签名的随机串
+    signature: '',// 必填，签名，见附录1
+    jsApiList: [] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+});
+
+```
+3、通过ready接口处理成功验证
+```javascript
+
+wx.ready(function(){
+
+    // config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready函数中。
+});
+```
+
+4、分享功能
+```javascript
+wx.ready(function(){
+    // 初始化wx对象
+    WeixinApi.ready(wx);
+	
+		// 微信分享的数据
+        var wxData = {
+            "imgUrl" : "http://www.jiayouxiaogui.cn/images/logo.png",// 分享图标
+            "link" : "http://www.jiayouxiaogui.cn", //分享链接
+            "desc" : "家有小鬼", //分享描述
+            "title" : "家有小鬼" // 分享标题
+        };
+        
+        // 分享的回调函数
+        var wxCallbacks={
+        	success:function(){
+        	  // 用户确认分享后执行的回调函数
+        		alert("success");
+        	},
+        	cancel:function(){
+        	  // 用户取消分享后执行的回调函数
+        		alert("cancel");
+        	},
+  		    fail:function(){
+  		    	//接口调用失败时执行的回调函数
+  		    },
+  		    complete:function(){
+  		    	// 接口调用完成时执行的回调函数，无论成功或失败都会执行。
+  		    },
+  		    trigger:function(){
+  		    	//监听Menu中的按钮点击时触发的方法，该方法仅支持Menu中的相关接口。 
+  		    }
+        };
+	
+		    // 用户点开右上角popup菜单后，点击分享给好友，会执行下面这个代码
+        WeixinApi.shareToFriend(wxData, wxCallbacks);
+
+        // 点击分享到朋友圈，会执行下面这个代码
+        WeixinApi.shareToTimeline(wxData, wxCallbacks);
+
+        // 点击分享到腾讯微博，会执行下面这个代码
+        WeixinApi.shareToWeibo(wxData, wxCallbacks);
+        
+        //点击分享到QQ，会执行下面这个代码
+		    WeixinApi.shareToQQ(wxData, wxCallbacks);
+	}
+```
+5、隐藏右上角option menu入口
+```javascript
+wx.ready(function(){
+  //隐藏右上角的option menu
+  WeixinApi.hideOptionMenu();
+  //显示
+  //WeixinApi.showOptionMenu();
+  //批量隐藏功能按钮接口
+  //WeixinApi.hideMenuItems();
+  //批量显示功能按钮接口
+  //WeixinApi.showMenuItems();
+  //隐藏所有非基础按钮接口
+  //WeixinApi.hideAllNonBaseMenuItem();
+  //显示所有功能按钮接口
+  //WeixinApi.showAllNonBaseMenuItem();
+}
+```
+
+6、隐藏底部工具栏
+
+```javascript
+wx.ready(function(){
+  // 隐藏
+	WeixinApi.hideToolbar();
+	
+	// 显示
+	// WeixinApi.showToolbar();
+}
+```
+
---
+++ WeixinApi.js
@@ -0,0 +1,457 @@
+(function (window) {
+
+    /**
+     * 定义WeixinApi
+     */
+    var WeixinApi = {
+        version: 1.0
+    };
+
+    var menuItems = ["menuItem:exposeArticle", "menuItem:setFont", "menuItem:dayMode", "menuItem:nightMode", "menuItem:refresh", "menuItem:profile",
+        //"menuItem:addContact",
+        "menuItem:share:appMessage", "menuItem:share:timeline", "menuItem:share:qq", "menuItem:share:weiboApp", "menuItem:favorite", "menuItem:share:facebook",
+        //"menuItem:jsDebug","menuItem:editTag","menuItem:delete","menuItem:originPage",
+        "menuItem:copyUrl", "menuItem:readMode",
+        //"menuItem:openWithQQBrowser","menuItem:openWithSafari","menuItem:share:email","menuItem:share:brand"
+    ];
+
+    window.WeixinApi = WeixinApi;
+
+    if (typeof define === 'function' && (define.amd || define.cmd)) {
+        if (define.amd) {
+            // AMD 规范，for：requirejs
+            define(function () {
+                return WeixinApi;
+            });
+        } else if (define.cmd) {
+            // CMD 规范，for：seajs
+            define(function (require, exports, module) {
+                module.exports = WeixinApi;
+            });
+        }
+    }
+
+    var _wx;
+
+    /**
+     * 初始化函数
+     */
+    WeixinApi.ready = function (wx) {
+        _wx = wx;
+    };
+
+    /**
+     * 判断当前客户端版本是否支持指定JS接口
+     * apiList:需要检测的JS接口列表
+     * callback:以键值对的形式返回，可用的api值true，不可用为false;如：{"checkResult":{"chooseImage":true},"errMsg":"checkJsApi:ok"}
+     */
+    WeixinApi.checkJsApi = function (apiList, callback) {
+        if (null == _wx) {
+            alert("微信接口对象不能为null");
+            return;
+        }
+
+        _wx.checkJsApi({
+            jsApiList: apiList, // 需要检测的JS接口列表，所有JS接口列表见附录2,
+            success: function (res) {
+                callback(res);
+            }
+        });
+    };
+
+    /**
+     * 分享给微信好友
+     * wxData: 待分享的信息
+     * wxData.title        分享的标题
+     * wxData.desc        描述
+     * wxData.imgUrl    图片地址
+     * wxData.link        链接地址
+     * wxData.type        享类型,music、video或link，不填默认为link
+     * wxData.dataUrl    如果type是music或video，则要提供数据链接，默认为空
+     *
+     * wxCallback:相关回调方法
+     * wxCallback.success        分享成功的回调函数
+     * wxCallback.cancel        取消分享的回调函数
+     * wxCallback.fail            失败时执行的回调函数
+     * wxCallback.complete        调用完成时执行的回调函数，无论成功或失败都会执行。
+     * wxCallback.trigger        监听Menu中的按钮点击时触发的方法，该方法仅支持Menu中的相关接口。
+     */
+    WeixinApi.shareToFriend = function (wxData, wxCallback) {
+        if (null == _wx) {
+            alert("微信接口对象不能为null");
+            return;
+        }
+        _wx.onMenuShareAppMessage({
+            title: wxData.title, // 分享标题
+            desc: wxData.desc, // 分享描述
+            link: wxData.link, // 分享链接
+            imgUrl: wxData.imgUrl, // 分享图标
+            type: wxData.type || 'link', // 分享类型,music、video或link，不填默认为link
+            dataUrl: wxData.dataUrl || '', // 如果type是music或video，则要提供数据链接，默认为空
+            success: function () {
+                // 用户确认分享后执行的回调函数
+                wxCallback.success();
+            },
+            cancel: function () {
+                // 用户取消分享后执行的回调函数
+                wxCallback.cancel();
+            },
+            fail: function () {
+                //接口调用失败时执行的回调函数
+                wxCallback.fail();
+            },
+            complete: function () {
+                // 接口调用完成时执行的回调函数，无论成功或失败都会执行。
+                wxCallback.complete();
+            },
+            trigger: function () {
+                //监听Menu中的按钮点击时触发的方法，该方法仅支持Menu中的相关接口。
+                wxCallback.complete();
+            }
+        });
+
+    };
+
+    /**
+     * 分享到微信朋友圈
+     * wxData: 待分享的信息
+     * wxData.title        分享的标题
+     * wxData.desc        描述
+     * wxData.imgUrl    图片地址
+     * wxData.link        链接地址
+     * wxData.type        享类型,music、video或link，不填默认为link
+     * wxData.dataUrl    如果type是music或video，则要提供数据链接，默认为空
+     *
+     * wxCallback:相关回调方法
+     * wxCallback.success        分享成功的回调函数
+     * wxCallback.cancel        取消分享的回调函数
+     * wxCallback.fail            失败时执行的回调函数
+     * wxCallback.complete        调用完成时执行的回调函数，无论成功或失败都会执行。
+     * wxCallback.trigger        监听Menu中的按钮点击时触发的方法，该方法仅支持Menu中的相关接口。
+     */
+    WeixinApi.shareToTimeline = function (wxData, wxCallback) {
+        if (null == _wx) {
+            alert("微信接口对象不能为null");
+            return;
+        }
+
+        _wx.onMenuShareTimeline({
+            title: wxData.title, // 分享标题
+            link: wxData.link, // 分享链接
+            imgUrl: wxData.imgUrl, // 分享图标
+            success: function () {
+                // 用户确认分享后执行的回调函数
+                wxCallback.success();
+            },
+            cancel: function () {
+                // 用户取消分享后执行的回调函数
+                wxCallback.cancel();
+            },
+            fail: function () {
+                //接口调用失败时执行的回调函数
+                wxCallback.fail();
+            },
+            complete: function () {
+                // 接口调用完成时执行的回调函数，无论成功或失败都会执行。
+                wxCallback.complete();
+            },
+            trigger: function () {
+                //监听Menu中的按钮点击时触发的方法，该方法仅支持Menu中的相关接口。
+                wxCallback.complete();
+            }
+        });
+    };
+
+    /**
+     * 分享到腾讯微博
+     * wxData: 待分享的信息
+     * wxData.title        分享的标题
+     * wxData.desc        描述
+     * wxData.imgUrl    图片地址
+     * wxData.link        链接地址
+     * wxData.type        享类型,music、video或link，不填默认为link
+     * wxData.dataUrl    如果type是music或video，则要提供数据链接，默认为空
+     *
+     * wxCallback:相关回调方法
+     * wxCallback.success        分享成功的回调函数
+     * wxCallback.cancel        取消分享的回调函数
+     * wxCallback.fail            失败时执行的回调函数
+     * wxCallback.complete        调用完成时执行的回调函数，无论成功或失败都会执行。
+     * wxCallback.trigger        监听Menu中的按钮点击时触发的方法，该方法仅支持Menu中的相关接口。
+     */
+    WeixinApi.shareToWeibo = function (wxData, wxCallback) {
+        if (null == _wx) {
+            alert("微信接口对象不能为null");
+            return;
+        }
+
+        _wx.onMenuShareWeibo({
+            title: wxData.title, // 分享标题
+            desc: wxData.desc, // 分享描述
+            link: wxData.link, // 分享链接
+            imgUrl: wxData.imgUrl, // 分享图标
+            success: function () {
+                // 用户确认分享后执行的回调函数
+                wxCallback.success();
+            },
+            cancel: function () {
+                // 用户取消分享后执行的回调函数
+                wxCallback.cancel();
+            },
+            fail: function () {
+                //接口调用失败时执行的回调函数
+                wxCallback.fail();
+            },
+            complete: function () {
+                // 接口调用完成时执行的回调函数，无论成功或失败都会执行。
+                wxCallback.complete();
+            },
+            trigger: function () {
+                //监听Menu中的按钮点击时触发的方法，该方法仅支持Menu中的相关接口。
+                wxCallback.complete();
+            }
+        });
+    };
+
+    /**
+     * 分享到QQ
+     * wxData: 待分享的信息
+     * wxData.title        分享的标题
+     * wxData.desc        描述
+     * wxData.imgUrl    图片地址
+     * wxData.link        链接地址
+     * wxData.type        享类型,music、video或link，不填默认为link
+     * wxData.dataUrl    如果type是music或video，则要提供数据链接，默认为空
+     *
+     * wxCallback:相关回调方法
+     * wxCallback.success        分享成功的回调函数
+     * wxCallback.cancel        取消分享的回调函数
+     * wxCallback.fail            失败时执行的回调函数
+     * wxCallback.complete        调用完成时执行的回调函数，无论成功或失败都会执行。
+     * wxCallback.trigger        监听Menu中的按钮点击时触发的方法，该方法仅支持Menu中的相关接口。
+     */
+    WeixinApi.shareToQQ = function (wxData, wxCallback) {
+        if (null == _wx) {
+            alert("微信接口对象不能为null");
+            return;
+        }
+
+        _wx.onMenuShareQQ({
+            title: wxData.title, // 分享标题
+            desc: wxData.desc, // 分享描述
+            link: wxData.link, // 分享链接
+            imgUrl: wxData.imgUrl, // 分享图标
+            success: function () {
+                // 用户确认分享后执行的回调函数
+                wxCallback.success();
+            },
+            cancel: function () {
+                // 用户取消分享后执行的回调函数
+                wxCallback.cancel();
+            },
+            fail: function () {
+                //接口调用失败时执行的回调函数
+                wxCallback.fail();
+            },
+            complete: function () {
+                // 接口调用完成时执行的回调函数，无论成功或失败都会执行。
+                wxCallback.complete();
+            },
+            trigger: function () {
+                //监听Menu中的按钮点击时触发的方法，该方法仅支持Menu中的相关接口。
+                wxCallback.complete();
+            }
+        });
+    };
+
+    /**
+     * 隐藏右上角菜单接口
+     */
+    WeixinApi.hideOptionMenu = function () {
+        if (null == _wx) {
+            alert("微信接口对象不能为null");
+            return;
+        }
+        _wx.hideOptionMenu();
+    };
+
+    /**
+     * 显示右上角菜单接口
+     */
+    WeixinApi.showOptionMenu = function () {
+        if (null == _wx) {
+            alert("微信接口对象不能为null");
+            return;
+        }
+        _wx.showOptionMenu();
+    };
+
+    /**
+     * 关闭当前网页窗口接口
+     */
+    WeixinApi.closeWindow = function () {
+        if (null == _wx) {
+            alert("微信接口对象不能为null");
+            return;
+        }
+        _wx.closeWindow();
+    };
+
+    /**
+     * 批量隐藏功能按钮接口
+     */
+    WeixinApi.hideMenuItems = function () {
+        if (null == _wx) {
+            alert("微信接口对象不能为null");
+            return;
+        }
+        _wx.hideMenuItems({
+            menuList: menuItems // 要隐藏的菜单项，所有menu项见附录3
+        });
+    };
+
+    /**
+     * 批量显示功能按钮接口
+     */
+    WeixinApi.showMenuItems = function () {
+        if (null == _wx) {
+            alert("微信接口对象不能为null");
+            return;
+        }
+        _wx.showMenuItems({
+            menuList: menuItems // 要显示的菜单项，所有menu项见附录3
+        });
+    };
+
+    /**
+     * 隐藏所有非基础按钮接口
+     */
+    WeixinApi.hideAllNonBaseMenuItem = function () {
+        if (null == _wx) {
+            alert("微信接口对象不能为null");
+            return;
+        }
+        _wx.hideAllNonBaseMenuItem();
+    };
+
+    /**
+     * 显示所有功能按钮接口
+     */
+    WeixinApi.showAllNonBaseMenuItem = function () {
+        if (null == _wx) {
+            alert("微信接口对象不能为null");
+            return;
+        }
+        _wx.showAllNonBaseMenuItem();
+    };
+
+    /**
+     * 显示底部工具栏
+     */
+    WeixinApi.showToolbar = function () {
+        WeixinJSBridge.call('showToolbar');
+    };
+
+    /**
+     * 隐藏底部工具栏
+     */
+    WeixinApi.hideToolbar = function () {
+        WeixinJSBridge.call('hideToolbar');
+    };
+
+    /**
+     * networkType; // 返回网络类型2g，3g，4g，wifi
+     */
+    WeixinApi.getNetworkType = function (callback) {
+        if (null == _wx) {
+            alert("微信接口对象不能为null");
+            return;
+        }
+
+        _wx.getNetworkType({
+            success: function (res) {
+                callback(res.networkType);
+            }
+        });
+    };
+
+    /**
+     * 拍照或从手机相册中选图接口
+     * callback([]):选择图片后的回调函数,返回选择的本地ID列表
+     *
+     */
+    WeixinApi.chooseImage = function (callback) {
+        if (null == _wx) {
+            alert("微信接口对象不能为null");
+            return;
+        }
+
+        _wx.chooseImage({
+            success: function (res) {
+                var localIds = res.localIds; // 返回选定照片的本地ID列表，localId可以作为img标签的src属性显示图片
+                callback(localIds);
+            }
+        });
+    };
+
+    /**
+     * 上传图片接口
+     * localId:String 需要上传的图片的本地ID，由chooseImage接口获得
+     * isShowProgressTips:int 显示进度提示(0-不显示，1-显示)，默认值为1
+     *
+     * callback(String):上传成功的服务器端图片id，通过该id调用downloadImage接口下载图片
+     */
+    WeixinApi.uploadImage = function (localId, isShowProgressTips, callback) {
+        if (null == _wx) {
+            alert("微信接口对象不能为null");
+            return;
+        }
+        _wx.uploadImage({
+            localId: localId, // 需要上传的图片的本地ID，由chooseImage接口获得
+            isShowProgressTips: isShowProgressTips || 1, // 默认为1，显示进度提示
+            success: function (res) {
+                var serverId = res.serverId; // 返回图片的服务器端ID
+                callback(serverId);
+            }
+        });
+    };
+
+    /**
+     * 下载图片接口
+     * serverId:String    需要下载的图片的服务器端ID，由uploadImage接口获得
+     * isShowProgressTips:int 显示进度提示(0-不显示，1-显示)，默认值为1
+     *
+     * callback(String):返回图片下载后的本地ID
+     */
+    WeixinApi.downloadImage = function (serverId, isShowProgressTips, callback) {
+        if (null == _wx) {
+            alert("微信接口对象不能为null");
+            return;
+        }
+        _wx.downloadImage({
+            serverId: serverId, // 需要下载的图片的服务器端ID，由uploadImage接口获得
+            isShowProgressTips: isShowProgressTips || 1, // 默认为1，显示进度提示
+            success: function (res) {
+                var localId = res.localId; // 返回图片下载后的本地ID
+                callback(localId);
+            }
+        });
+    };
+
+    /**
+     * 预览图片接口
+     * curSrc:当前显示图片的链接
+     * srcList：需要预览的图片链接列表
+     */
+    WeixinApi.previewImage = function (curSrc, srcList) {
+        if (null == _wx) {
+            alert("微信接口对象不能为null");
+            return;
+        }
+        _wx.previewImage({
+            current: curSrc, // 当前显示的图片链接
+            urls: srcList // 需要预览的图片链接列表
+        });
+    };
+
+})(window);
